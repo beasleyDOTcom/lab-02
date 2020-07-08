4bb3d1ea4ecc1a3c2dad1bce4974b7460a10366b
thatvetdevrob <thatvetdevrob@gmail.com>
2020-07-07 17:45:17 -0700

    Feature 1 and a half

--- js/app.js
+++ js/app.js
@@ -1,18 +1,33 @@
+const arr = [];
+
 // GOAL: get the information from the data.json file, fill the template and display to the page
 
 
-console.log("in app.js");
 
 // use ajax to get the information from data.json file
 $.ajax('page-1.json', {method: 'GET', dataType: 'JSON'})
   .then(data => {
-    console.log('what we got back from ajax', data);
     // run the information through a constructor function in order to normalize the data
 
+
     data.forEach(objectInDataArray => {
-      console.log('My animals from the images', objectInDataArray);
       new Pic(objectInDataArray).picBuilder();
+
+
+      if(arr.includes(objectInDataArray.keyword) === false){
+        
+        arr.push(objectInDataArray.keyword);
+        console.log('This is our arr at real time: '+ arr);
+
+
+        $( 'select' ).append( '<option>'+objectInDataArray.keyword+'</option>' );
+
+      }
+
     });
+
+
+
   });
 
 function Pic(obj){
@@ -35,6 +50,8 @@ Pic.prototype.picBuilder = function(){
   // create a new section to hold my new template
   const $newSection = $(`<section>${myTemplate}</section>`);
 
+  $newSection.attr('class', this.keyword);
+
   // find the h2 tag and fill it with the name of the dog
   $newSection.find('h2').text(this.title);
 
@@ -44,9 +61,52 @@ Pic.prototype.picBuilder = function(){
   // find the img and put in the imgage url
   $newSection.find('img').attr('src', this.image_url);
 
+  let val = this.keyword;
+
   // find the img and add in the alt
-  $newSection.find('img').attr('alt', this.title);
+  $newSection.find('img').attr('alt', this.title) ;
+
+
 
   $('main').append($newSection);
 
+
+
+
 };
+
+$(document).ready(function(){
+
+
+  $("select").change(function(){
+
+      var selected = $(this).children("option:selected").val();
+
+      $('section').removeClass(selected);
+
+      alert("You have selected " + selected);
+
+  });
+
+});
+
+// async code
+// $('select').on('click', 'option', function(){
+//   $('option').removeClass('select'.value);
+//   console.log('this is our this:', this);
+//   // 'this' is the event.target
+// });
+
+// $('#form').submit((event) => {
+//   event.preventDefault();
+//   console.log('form submitted');
+// });
+
+// // non-ansync code needs to go in here to wait for the page to be ready before it can run
+// $().ready(
+//   console.log( 'ready!' )
+// );
+
+
+// how do we make it so that EVERY <a> console.logs() the text?
+console.log("This is our last line of code: "+ arr);
